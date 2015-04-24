384c541717ad3ce573b31c6957bbd9ab29f7cab3
Javier G. Sogo <jgsogo@gmail.com>
2015-04-24 10:27:34 +0200

    comments

--- sokoban/level1.pl
+++ sokoban/level1.pl
@@ -1,15 +1,24 @@
-% sokoban\level1.pl: puzzle#1
+
+% sokoban\level1.pl: board layout for Sokoban puzzle
 %     ... learning Prolog with SWI-Prolog
-% by Javier G. Sogo
+% Author: Javier G. Sogo
 
+/***************************************************************************/
+/* Sokoban board: level 1                                                  */
+/***************************************************************************/
+/* It is a 3x3 board with two boxes and two storage positions              */
+/*                                                                         */
+/*  +---+---+---+                                                          */
+/*  | S |   |   |                                                          */
+/*  +---+---+---+                                                          */
+/*  |   | X | X |                                                          */
+/*  +---+---+---+                                                          */
+/*  |   | · | · |                                                          */
+/*  +---+---+---+                                                          */
+/*                                                                         */
+/***************************************************************************/
 
-%:-include(board).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Define level 1
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% 3x3 Board
+/* Board definition: adjacent positions                                    */
 top(x1y3,x1y2).
 top(x1y2,x1y1).
 top(x2y3,x2y2).
@@ -24,17 +33,16 @@ right(x2y2, x3y2).
 right(x1y1, x2y1).
 right(x2y1, x3y1).
 
-% 2 boxes
+/* Boxes initial positions                                                 */
 box(x2y2).
 box(x3y2).
 
-% solution
+/* Storage positions                                                       */
 solution(x2y1).
 solution(x3y1).
 
-% sokoban
+/* Sokoban initial position                                                */
 sokoban(x1y1).
 
-
-% Initial state assertion
+/* Initial state assertion                                                 */
 initial_state(sokoban, state(x1y1, [x2y2, x3y2])).
--- sokoban/sokoban.pl
+++ sokoban/sokoban.pl
@@ -1,29 +1,31 @@
-% sokoban\sokoban.pl: Sokoban puzzle
+
+% sokoban\sokoban.pl: depth-first search implementation (applied to Sokoban puzzle)
 %     ... learning Prolog with SWI-Prolog
-% by Javier G. Sogo
+% Author: Javier G. Sogo
 
+/***************************************************************************/
+/* Depth-first search solving framework                                    */
+/***************************************************************************/
+/* Depth-first search (DFS) is an algorithm for traversing or searching    */
+/* tree or graph data structures. This algorithm can be applied to         */
+/* combinatorial problems and guarantees to find the optimal solution      */
+/* as it traverses the entire graph. Nevertheless for large graphs time    */
+/* and/or resources may be a limitation.                                   */
+/*                                                                         */
+/***************************************************************************/
 
-:-include(level1).
-:-include(game).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Play Sokoban: 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% DFS solving framework
-
-% The problem is solved if the state is equal to final_state.
+/* The problem is solved if the state is equal to final_state.             */
 solve_dfs(Problem, State, _History, []) :-
     final_state(Problem, State).
 
-% If not, we have to explore new states
+/* If not, we have to explore new states                                   */
 solve_dfs(Problem, State, History, [Move|Moves]) :-
     movement(State, Move),
     update(State, Move, NewState),
     \+ member(NewState, History),   /* No quiero ciclos en el grafo de búsqueda */
     solve_dfs(Problem, NewState, [NewState|History], Moves).
 
-% Actually solve the problem
+/* Actually solve the problem                                              */
 solve_problem(Problem, Solution) :-
     format('=============~n'),
     format('|| Problem: ~w~n', Problem),
@@ -33,7 +35,19 @@ solve_problem(Problem, Solution) :-
     solve_dfs(Problem, Initial, [Initial], Solution).
 
 
-% Clear screen
+/***************************************************************************/
+/* Application to Sokoban: include game rules and board layout             */
+/***************************************************************************/
+
+/* Game rules                                                              */
+:-include(game).
+
+/* Modify level file include to play other boards                          */
+:-include(level1).
+
+/***************************************************************************/
+/* Utility rules                                                           */
+/***************************************************************************/
 clear_screen :-
     format('~c~s', [0x1b, "[2J"]).  /* Credit: http://stackoverflow.com/questions/16908764/clearing-screen-in-swipl-prolog-in-windows */
 
